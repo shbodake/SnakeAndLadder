ba48b831bcd369e4713597a6912dd8cf57742ecf
shbodake <sayalibodake28@gmail.com>
2021-10-21 12:44:34 +0530

    Added: Welcome message added

---
+++ src/com/blp/snakeandladder/SnakeAndLadder.java
@@ -0,0 +1,7 @@
+package com.blp.snakeandladder;
+
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake And Ladder program");
+    }
+}

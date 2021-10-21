4e86e6e6911de844486af1dcb13f6ef84b0177e0
shbodake <sayalibodake28@gmail.com>
2021-10-21 12:48:41 +0530

    Added: Player starting position

--- src/com/blp/snakeandladder/SnakeAndLadder.java
+++ src/com/blp/snakeandladder/SnakeAndLadder.java
@@ -3,5 +3,7 @@ package com.blp.snakeandladder;
 public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake And Ladder program");
+        int position = 0;
+        System.out.println("The Starting Position of Player is : " + position);
     }
 }

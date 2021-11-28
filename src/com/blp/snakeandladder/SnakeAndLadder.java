f71209ece80d633907225306db57ba5a4a301da2
shbodake <sayalibodake28@gmail.com>
2021-11-28 16:15:59 +0530

    DDED: Player reaches winning position

--- src/com/blp/snakeandladder/SnakeAndLadder.java
+++ src/com/blp/snakeandladder/SnakeAndLadder.java
@@ -12,8 +12,6 @@ public class SnakeAndLadder {
         while (position < WINNING_POSITION) {
             int diceNumber = (int) (Math.floor(Math.random() * 10) % 6 + 1);
             System.out.println("DiceNumber is : " + diceNumber);
-            System.out.println("The Starting Position of Player is : " + position);
-            System.out.println("Player Roll the Dice and get DiceNumber : " + diceNumber);
 
             int option = (int) (Math.floor(Math.random() * 10) % 3 + 1);
             switch (option) {
